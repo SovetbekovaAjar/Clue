850f8887c4f6b2d393e325e799c29c72f7e01089
Nazikos <nazar.argymbayev@mail.ru>
2023-05-04 01:16:52 +0600

    Nazilos

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,9 @@
-export const BASE_URL = "http://13.50.246.163/auth";
+export const BASE_URL = "http://13.50.246.163";
 
 export const ACTIONS = {
   user: "user",
+  products: "products",
+  categories: "categories",
+  oneProduct: "oneProduct",
+  pageTotalCount: "pageTotalCount",
 };
