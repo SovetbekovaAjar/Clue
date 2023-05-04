e613ec17677866e0bc44262d45fba11cbae27b3c
Your Name <sovetbekovaajar@gmail.com>
2023-05-04 19:48:50 +0600

    init

--- src/components/Masonry.jsx
+++ src/components/Masonry.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Masonry() {
-  return <div className="grid">(.grid-item)</div>;
+  return (
+    <div className="grid">
+      <div className="grid-item">
+        <img src="img/img1.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img2.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img3.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img4.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img5.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img6.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img7.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img8.jpg" alt="" />
+      </div>
+      <div className="grid-item">
+        <img src="img/img9.jpg" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Masonry;
--- src/pages/MinPage.jsx
+++ src/pages/MinPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppNar from "../components/AppNar";
+import Masonry from "../components/Masonry";
 
 function MinPage() {
   return (
     <div>
       <AppNar />
+      <Masonry />
     </div>
   );
 }
